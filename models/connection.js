6d741a135ae66f76a5b2d39de556efdde84d5259
Nico <nicolas.callaghan@gmail.com>
2025-07-18 16:21:47 +0200

    env ok

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// const connectionString = process.env.CONNECTION_STRING;
+const connectionString = process.env.CONNECTION_STRING;
 
-const connectionString='mongodb+srv://nicolascallaghan:RNfXlWUwO2Ymow1p@cluster0.2wp4bta.mongodb.net/hackatweet'
+
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
